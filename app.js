71a905ff31fdbd586ad7e7e9cead7f4a31a6bd1a
Pranav K <pranavkcse@gmail.com>
2023-10-16 20:54:06 +0530

    updated code

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
+require('dotenv').config()
+
 const express = require('express');
-const mongoose = require('mongoose');
+const connectDB = require('./server/config/db')
 const authRoutes = require('./server/routes/authRoutes')
 
 const app = express();
+const PORT = 3000 || process.env.PORT;
+
+// DB connection
+connectDB();
 
 // middleware
 app.use(express.static('public'));
 
+// middleware to pass data
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 // view engine
 app.set('view engine', 'ejs');
 
-// database connection
-
-const dbURI = 'mongodb+srv://pranavkcse:KiagGBD6JTxOf5Kn@cluster0.twouuza.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on : http://localhost:${PORT}`);
+})
--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,12 +1,43 @@
+const { log } = require('console');
+const User = require('../models/User')
+
+// Handle Error
+const handleError = (error) => {
+    console.log(error.message, error.code);
+    let errors = { email: "", password: ''}
+
+    // Validation errors
+    if (error.message.includes('user validation failed')){
+        Object.values(error.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message;
+        })
+    }
+
+    return errors;
+}
+
+
 exports.signup_get = (req,res)=> {
     res.render('signup')
 }
+
 exports.login_get = (req,res)=> {
     res.render('login')
 }
-exports.signup_post = (req,res)=> {
-    res.send('new signup')
+
+exports.signup_post = async (req,res)=> {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.create({email, password});
+        res.status(201).json(user);
+    } catch (error) {
+        const errors = handleError(error);
+        res.status(400).json({ errors });
+    }
+
 }
-exports.login_post = (req,res)=> {
+
+exports.login_post = async (req,res)=> {
     res.send('user login')
 }
